2746f1832b773038b3428e252585ebb39bdf20f1
tafsiruzzaman <tafsirjaman100@gmail.com>
2021-11-02 20:55:44 +0600

    project started and some necessary package installation complete

---
+++ src/pages/Home/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../../Shared/Navigation/Navigation';
+
+const Home = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+            <h1>home</h1>
+        </div>
+    );
+};
+
+export default Home;
